a0a2d5ff035e3dff88a7e7233b928275381c2b3e
Michael Rees <reesmichael1@vivaldi.net>
2018-10-30 18:20:34 -0500

    Improve filepath extraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 use std::fs;
+use std::path::{Path, PathBuf};
+use std::ffi::OsStr;
 
 extern crate pdf_canvas;
 
@@ -28,13 +30,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
     let tree = parser::parse(&contents)?;
     
-    writer::write_document(tree, &extract_path(&config.filename));
+    writer::write_document(tree, &extract_path(&config.filename)?);
     Ok(())
 }
 
-fn extract_path(source: &str) -> String {
-    // Obviously do something better here when I have Internet (currently on airplane)
-    source.replace(".bur", ".pdf")
+fn extract_path(source: &str) -> Result<String, String> {
+    let path = Path::new(source);
+
+    let parent = path.parent();
+    let file_stem = path.file_stem();
+    let extension = path.extension();
+
+    if extension != Some(OsStr::new("bur")) {
+        return Err(format!("{} has an unsupported filetype", source));
+    }
+
+    let mut path = PathBuf::from(parent.unwrap());
+    path.push(file_stem.unwrap());
+    path.set_extension("pdf");
+
+    Ok(String::from(path.to_str().unwrap()))
 }
 
 #[cfg(test)]
@@ -42,10 +57,10 @@ mod test {
     use super::*;
 
     #[test]
-    fn extract_path_is_correct() {
-        let path = "/abc/123/source.bur";
-        let expected = String::from("/abc/123/source.pdf");
-        let result = extract_path(path);
-        assert_eq!(result, expected);
+    fn extract_path_correct() {
+        let path: PathBuf = ["abc", "123", "source.bur"].iter().collect();
+        let expected: PathBuf = ["abc", "123", "source.pdf"].iter().collect();
+        let result = extract_path(&String::from(path.to_str().unwrap())).unwrap();
+        assert_eq!(result, String::from(expected.to_str().unwrap()));
     }
 }
